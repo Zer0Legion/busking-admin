7ada8c8ecfc429dbebdab458a9a7401d7c4f9c70
Ang Leng Khai <lengkhai@gmail.com>
2023-08-01 15:29:06 +0800

    Update next.config.js

--- busking-admin/next.config.js
+++ busking-admin/next.config.js
@@ -2,13 +2,5 @@
 const nextConfig = {}
 
 module.exports = {
-    webpack5: true,
-    webpack: (config) => {
-      config.resolve.fallback = { 
-        fs: false,
-        path: false
-    };
-  
-      return config;
-    },
+    
   };
